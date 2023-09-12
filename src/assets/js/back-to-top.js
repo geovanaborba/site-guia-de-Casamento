7c39a50bbba7362a7a61140ceb99cacb331a37fe
Geovana Borba <geborba19@hotmail.com>
2023-09-12 11:52:53 -0300

    adicionada função para que botão de voltar ao topo apareça apenas depois de rolar 1500px da página

--- src/assets/js/back-to-top.js
+++ src/assets/js/back-to-top.js
@@ -1,5 +1,18 @@
-var btn = document.querySelector("#back-to-top");
+var btn = document.querySelector('#back-to-top');
 
-btn.addEventListener("click", function() {
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+  }
+}
+
+
+btn.addEventListener('click', function() {
     window.scrollTo(0, 0);
 });
